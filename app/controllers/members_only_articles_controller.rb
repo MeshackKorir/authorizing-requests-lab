48b8c62313b8a18499200d8feafefe6d0e13ab53
Meshack Korir <meshackkorir100@gmail.com>
2023-07-01 22:49:48 +0300

    done

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,20 +1,35 @@
 class MembersOnlyArticlesController < ApplicationController
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authenticate_user, only: [:index, :show]
 
   def index
-    articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
-    render json: articles, each_serializer: ArticleListSerializer
+    if user_signed_in?
+      articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
+      render json: articles, each_serializer: ArticleListSerializer
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 
   def show
-    article = Article.find(params[:id])
-    render json: article
+    if user_signed_in?
+      article = Article.find(params[:id])
+      render json: article
+    else
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 
   private
 
-  def record_not_found
-    render json: { error: "Article not found" }, status: :not_found
+  def authenticate_user
+    unless user_signed_in?
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
   end
 
+  def user_signed_in?
+    # Replace with your logic to check if the user is signed in
+    # For example, you can use Devise's current_user helper
+    current_user.present?
+  end
 end
